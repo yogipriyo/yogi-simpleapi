450eca7a54dcaaed5993732037985344ad5580c7
yogi bear <yogi@softwareseni.com>
2015-12-01 10:53:36 +0700

    delete 2 files and prepare for heroku push

--- go-postgre.go
+++ go-postgre.go
@@ -1,166 +1,168 @@
 package main
+
 import (
- 	"github.com/gin-gonic/gin"
- 	"database/sql"
- 	"gopkg.in/gorp.v1"
- 	_ "github.com/lib/pq"
- 	"strconv"
- 	"log"
- 	//"fmt"
- 	//"reflect"
+	"database/sql"
+	"github.com/yogipriyo/Simple-Gin-Gonic-API/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	_ "github.com/yogipriyo/Simple-Gin-Gonic-API/Godeps/_workspace/src/github.com/lib/pq"
+	"github.com/yogipriyo/Simple-Gin-Gonic-API/Godeps/_workspace/src/gopkg.in/gorp.v1"
+	"log"
+	"strconv"
+	//"fmt"
+	//"reflect"
 )
+
 ////
 
 var dbmap = initDb()
 
 func initDb() *gorp.DbMap {
- 	db, err := sql.Open("postgres", "postgres://postgres:09030015@localhost/gopgtest")
- 	checkErr(err, "sql.Open failed")
- 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
- 	dbmap.AddTableWithName(User{}, "User2").SetKeys(true, "Id")
- 	err = dbmap.CreateTablesIfNotExists()
- 	checkErr(err, "Create table failed")
+	db, err := sql.Open("postgres", "postgres://postgres:09030015@localhost/gopgtest")
+	checkErr(err, "sql.Open failed")
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+	dbmap.AddTableWithName(User{}, "User2").SetKeys(true, "Id")
+	err = dbmap.CreateTablesIfNotExists()
+	checkErr(err, "Create table failed")
 	return dbmap
 }
 
 func checkErr(err error, msg string) {
- 	if err != nil {
- 		log.Fatalln(msg, err)
- 	}
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
 }
+
 ////
 
 func GetUsers(c *gin.Context) {
- 	var users []User
- 	_, err := dbmap.Select(&users, "SELECT * FROM user2")
+	var users []User
+	_, err := dbmap.Select(&users, "SELECT * FROM user2")
 	if err == nil {
- 		c.JSON(200, users)
- 	} else {
- 		c.JSON(404, gin.H{"error": "no user(s) into the table"})
- 	}
+		c.JSON(200, users)
+	} else {
+		c.JSON(404, gin.H{"error": "no user(s) into the table"})
+	}
 	// curl -i http://localhost:8080/api/v1/users
 }
 
 func GetUser(c *gin.Context) {
- 	param_id := c.Params.ByName("id")
- 	id, _ := strconv.Atoi(param_id)
- 	//original//
- 	//var user User
- 	//err := dbmap.SelectOne(&user, "SELECT * FROM user2 WHERE id=?", id)
- 	//new//
- 	obj, err := dbmap.Get(User{}, id)
- 	//c.JSON(404, gin.H{"obj" : obj})
+	param_id := c.Params.ByName("id")
+	id, _ := strconv.Atoi(param_id)
+	//original//
+	//var user User
+	//err := dbmap.SelectOne(&user, "SELECT * FROM user2 WHERE id=?", id)
+	//new//
+	obj, err := dbmap.Get(User{}, id)
+	//c.JSON(404, gin.H{"obj" : obj})
 	user := obj.(*User)
 	if err == nil {
- 		user_id, _ := strconv.ParseInt(param_id, 0, 64)
+		user_id, _ := strconv.ParseInt(param_id, 0, 64)
 		content := &User{
- 			Id: user_id,
- 			Firstname: user.Firstname,
- 			Lastname: user.Lastname,
- 		}
- 		c.JSON(200, content)
- 	} else {
- 		c.JSON(404, gin.H{"error": "user not found"})
- 		//c.JSON(404, gin.H{"error" : err})
- 	}
+			Id:        user_id,
+			Firstname: user.Firstname,
+			Lastname:  user.Lastname,
+		}
+		c.JSON(200, content)
+	} else {
+		c.JSON(404, gin.H{"error": "user not found"})
+		//c.JSON(404, gin.H{"error" : err})
+	}
 	// curl -i http://localhost:8080/api/v1/users/1
 }
 
 func PostUser(c *gin.Context) {
 	var params User
- 	c.Bind(&params)
+	c.Bind(&params)
 
 	if params.Firstname != "" && params.Lastname != "" {
-		user := &User{0,params.Firstname,params.Lastname}
- 		err := dbmap.Insert(user)		 
- 		if err == nil {
+		user := &User{0, params.Firstname, params.Lastname}
+		err := dbmap.Insert(user)
+		if err == nil {
 			content := &User{
-				Id: user.Id,
+				Id:        user.Id,
 				Firstname: user.Firstname,
-				Lastname: user.Lastname,
+				Lastname:  user.Lastname,
 			}
 			c.JSON(201, content)
 		} else {
 			c.JSON(422, gin.H{"error": err})
 		}
 	} else {
- 		c.JSON(422, gin.H{"error": "fields are empty"})
- 	}
+		c.JSON(422, gin.H{"error": "fields are empty"})
+	}
 	// curl -i -X POST -H "Content-Type: application/json" -d "{ \"firstname\": \"Thea\", \"lastname\": \"Queen\" }" http://localhost:8080/api/v1/users
 }
 
 func UpdateUser(c *gin.Context) {
- 	id := c.Params.ByName("id")
- 	var user User
- 	err := dbmap.SelectOne(&user, "SELECT * FROM user2 WHERE id=?", id)
+	id := c.Params.ByName("id")
+	var user User
+	err := dbmap.SelectOne(&user, "SELECT * FROM user2 WHERE id=?", id)
 	if err == nil {
- 		var json User
- 		c.Bind(&json)
+		var json User
+		c.Bind(&json)
 		user_id, _ := strconv.ParseInt(id, 0, 64)
 		user := User{
-	 		Id: user_id,
-	 		Firstname: json.Firstname,
-	 		Lastname: json.Lastname,
-	 	}
-		if user.Firstname != "" && user.Lastname != ""{
-	 		_, err = dbmap.Update(&user)
+			Id:        user_id,
+			Firstname: json.Firstname,
+			Lastname:  json.Lastname,
+		}
+		if user.Firstname != "" && user.Lastname != "" {
+			_, err = dbmap.Update(&user)
 			if err == nil {
-	 			c.JSON(200, user)
-	 		} else {
-	 			checkErr(err, "Updated failed")
-	 		}
+				c.JSON(200, user)
+			} else {
+				checkErr(err, "Updated failed")
+			}
 		} else {
-	 		c.JSON(422, gin.H{"error": "fields are empty"})
-	 	}
+			c.JSON(422, gin.H{"error": "fields are empty"})
+		}
 	} else {
- 		c.JSON(404, gin.H{"error": "user not found"})
- 	}
+		c.JSON(404, gin.H{"error": "user not found"})
+	}
 	// curl -i -X PUT -H "Content-Type: application/json" -d "{ \"firstname\": \"Thea\", \"lastname\": \"Merlyn\" }" http://localhost:8080/api/v1/users/1
 }
 
 func DeleteUser(c *gin.Context) {
- 	id := c.Params.ByName("id")
+	id := c.Params.ByName("id")
 	var user User
- 	err := dbmap.SelectOne(&user, "SELECT id FROM user2 WHERE id=?", id)
+	err := dbmap.SelectOne(&user, "SELECT id FROM user2 WHERE id=?", id)
 	if err == nil {
-	 	_, err = dbmap.Delete(&user)
+		_, err = dbmap.Delete(&user)
 		if err == nil {
 			c.JSON(200, gin.H{"id #" + id: " deleted"})
-	 	} else {
-	 		checkErr(err, "Delete failed")
+		} else {
+			checkErr(err, "Delete failed")
 		}
 	} else {
- 		c.JSON(404, gin.H{"error": "user not found"})
- 	}
+		c.JSON(404, gin.H{"error": "user not found"})
+	}
 	// curl -i -X DELETE http://localhost:8080/api/v1/users/1
 }
 
-func index (c *gin.Context) {
-    content := gin.H{"Hello": "World"}
-    c.JSON(200, content)
+func index(c *gin.Context) {
+	content := gin.H{"Hello": "World"}
+	c.JSON(200, content)
 }
 
 ////
 type User struct {
- 	Id int64 `db:"id" json:"id"`
- 	Firstname string `db:"firstname" json:"firstname"`
- 	Lastname string `db:"lastname" json:"lastname"`
+	Id        int64  `db:"id" json:"id"`
+	Firstname string `db:"firstname" json:"firstname"`
+	Lastname  string `db:"lastname" json:"lastname"`
 }
 
 func main() {
- 	r := gin.Default()
+	r := gin.Default()
 	v1 := r.Group("api/v1")
- 	{
- 		v1.GET("/users", GetUsers)
- 		v1.GET("/users/:id", GetUser)
- 		v1.POST("/users", PostUser)
- 		v1.PUT("/users/:id", UpdateUser)
- 		v1.DELETE("/users/:id", DeleteUser)
- 	}
- 	r.GET("/", index)
+	{
+		v1.GET("/users", GetUsers)
+		v1.GET("/users/:id", GetUser)
+		v1.POST("/users", PostUser)
+		v1.PUT("/users/:id", UpdateUser)
+		v1.DELETE("/users/:id", DeleteUser)
+	}
+	r.GET("/", index)
 	r.Run(":8081")
 }
-
 
 //static example//
 // func GetUsers(c *gin.Context) {
